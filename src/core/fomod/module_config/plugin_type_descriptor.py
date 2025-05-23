4a7b79c52dc65767021c492d7babf2b624de5f5f
Cutleast <cutleast@gmail.com>
2025-05-23 14:41:40 +0200

    PluginTypeDescriptor: Add additional validation

--- src/core/fomod/module_config/plugin_type_descriptor.py
+++ src/core/fomod/module_config/plugin_type_descriptor.py
@@ -2,15 +2,16 @@
 Copyright (c) Cutleast
 """
 
-from typing import Optional
+from typing import Optional, Self
 
+from pydantic import model_validator
 from pydantic_xml import BaseXmlModel, element
 
 from .dependency.dependency_plugin_type import DependencyPluginType
 from .plugin_type import PluginType
 
 
-class PluginTypeDescriptor(BaseXmlModel, search_mode="unordered"):
+class PluginTypeDescriptor(BaseXmlModel):
     """
     Model representing a plugin type descriptor.
     """
@@ -25,3 +26,15 @@ class PluginTypeDescriptor(BaseXmlModel, search_mode="unordered"):
 
     type: Optional[PluginType] = element(name="type", default=None)
     """The type of the plugin. Mutual exclusive with `dependency_type`."""
+
+    @model_validator(mode="after")
+    def check_exclusivity(self) -> Self:
+        # TODO: Reevaluate this
+        if self.dependency_type is not None and self.type is not None:
+            raise ValueError("dependency_type and type are mutually exclusive!")
+        elif self.dependency_type is None and self.type is None:
+            raise ValueError(
+                "dependency_type and type are both missing but one is required!"
+            )
+
+        return self
