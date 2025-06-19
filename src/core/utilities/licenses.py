760b787a473e79df7a9378c31a51c718f0250b01
Cutleast <cutleast@gmail.com>
2025-06-19 17:46:38 +0200

    licenses.py: Add missing licenses

--- src/core/utilities/licenses.py
+++ src/core/utilities/licenses.py
@@ -3,14 +3,18 @@ Copyright (c) Cutleast
 """
 
 LICENSES: dict[str, str] = {
+    "comtypes": "https://github.com/enthought/comtypes/blob/main/LICENSE.txt",
     "cx_freeze": "https://github.com/marcelotduarte/cx_Freeze/blob/main/LICENSE.md",
     "darkdetect": "https://github.com/albertosottile/darkdetect/blob/master/LICENSE",
     "jstyleson": "https://github.com/linjackson78/jstyleson/blob/master/LICENSE",
+    "lxml": "https://github.com/lxml/lxml/blob/master/LICENSE.txt",
     "pydantic": "https://github.com/pydantic/pydantic/blob/main/LICENSE",
+    "pydantic-xml": "https://github.com/dapper91/pydantic-xml/blob/master/LICENSE",
     "PySide6": "https://code.qt.io/cgit/pyside/pyside-setup.git/tree/LICENSES?h=6.8.2",
     "pywin32": "https://github.com/mhammond/pywin32",
     "Qt6": "https://doc.qt.io/qt-6/lgpl.html",
     "qtawesome": "https://github.com/spyder-ide/qtawesome/blob/master/LICENSE.txt",
     "requests": "https://github.com/psf/requests/blob/main/LICENSE",
     "semantic-version": "https://github.com/rbarrois/python-semanticversion/blob/master/LICENSE",
+    "send2trash": "https://github.com/arsenetar/send2trash/blob/master/LICENSE",
 }
